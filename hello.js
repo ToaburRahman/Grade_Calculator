834e194d6eed0d7c7a5e390849334221e98af665
unknown <toabur20@gmail.com>
2023-06-15 00:08:28 +0600

    grade Calculator

---
+++ hello.js
@@ -0,0 +1,25 @@
+const yourMarks = 100;
+if(yourMarks >= 90 && yourMarks <= 100){
+    console.log("Congraculation : A+");
+}
+else if( yourMarks >= 80 && yourMarks <= 89){
+    console.log("Congraculation : A-");
+}
+else if(yourMarks >= 70 && yourMarks <- 79){
+    console.log("Congraculation : A- ");
+}
+else if(yourMarks >= 60 &&  yourMarks <= 69){
+    console.log("Congraculation : A ");
+}
+else if(yourMarks >= 50 && yourMarks <= 59){
+    console.log("Congraculation : B");
+}
+else if(yourMarks >= 40 && yourMarks <= 49){
+    console.log("Congraculation  : C");
+}
+else if(yourMarks >=33 && yourMarks <= 39){
+    console.log("Congraculation  : D");
+}
+else{
+    console.log("Congraculation You Are Faild ");
+}
